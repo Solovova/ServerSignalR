187aebf5e2e42d214db88dcc0c7bacccaaaa90cf
Solonichniy Volodimir <vsolo0@gmail.com>
2019-05-13 21:24:52 +0300

    Добавьте файлы проекта.

---
+++ ServerSignalR2/Hubs/MoveViewHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSignalR.Hubs
+{
+    public class MoveViewHub : Hub
+    {
+        public async Task MoveViewFromServer(string id, string sensor, string value)
+        {
+            Console.WriteLine("Receive:  ID -> |" + id + "|  Sensor: |" + sensor +"| Value: |" + value +"|");
+            float dX = 0.6f;
+            //float dY = 0.0f;
+            //string tS = "teeeest";
+
+            await Clients.Others.SendAsync("ReceiveNewPositions", id, sensor, value);
+            //await Clients.Others.SendAsync("ReceiveNewPositions",dX);
+        }
+
+    }
+}
